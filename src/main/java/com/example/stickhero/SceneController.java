817abf909f8e0c2bfd19c754de35e3cb890126dc
aturtle4 <armaansingh0404@gmail.com>
2023-11-16 01:10:42 +0530

    rearranged the file structure

--- src/main/java/com/example/stickhero/SceneController.java
+++ src/main/java/com/example/stickhero/SceneController.java
@@ -1,4 +1,4 @@
-package com.example.stickhero;
+package com.example.stickhero.Code;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SceneController extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StartScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SceneController.class.getResource("StartScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),300,600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
