ffcf3c1241f23a52791ce8ac6f858c5ef11b3a7a
aturtle4 <armaansingh0404@gmail.com>
2023-11-16 01:18:01 +0530

    aunt - I

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module com.example.stickhero {
     requires javafx.fxml;
 
 
-    opens com.example.stickhero to javafx.fxml;
     exports com.example.stickhero;
-    exports com.example.stickhero.Code;
-    opens com.example.stickhero.Code to javafx.fxml;
+    opens com.example.stickhero to javafx.fxml;
 }
