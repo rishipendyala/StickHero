3ee4c395567a9121b3b3d3faa6700545a184d5c3
Armaan Singh <123837862+aturtle4@users.noreply.github.com>
2023-12-08 20:04:28 +0000

    got translation and randogeneration to work

--- src/main/java/com/example/stickhero/Game.java
+++ src/main/java/com/example/stickhero/Game.java
@@ -1,24 +1,18 @@
 package com.example.stickhero;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.ParallelTransition;
-import javafx.animation.Timeline;
-import javafx.animation.TranslateTransition;
-import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.animation.*;
+
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.util.concurrent.atomic.AtomicReference;
-
-
-import static java.lang.Thread.sleep;
 
 public class Game  {
 
@@ -35,6 +29,8 @@ public class Game  {
     private static boolean inverted = true;
     private static Scene scene;
     private static boolean isrunning = false;
+    private static ImageView bg = null;
+    private static Pane pane ;
 
 
 
@@ -44,7 +40,6 @@ public class Game  {
         bg.setFitHeight(600.0);
         bg.setFitWidth(400.0);
         bg.setPickOnBounds(true);
-        T2 t2 = new T2();
         // Creating the first rectangle
         curr = new Rectangle();
         curr.setHeight(200);
@@ -62,35 +57,57 @@ public class Game  {
 
         sprite.setX(curr.getWidth() - 100);
         sprite.setY(600 - (curr.getHeight() + 75));
+
+         Timeline spriteAnimation = new Timeline(
+                 new KeyFrame(Duration.millis(100), event -> animateSprite(sprite))
+         );
+         T2 t2 = new T2();
+         if (secondary == null) {
+             secondary = createBlock();
+             double randomXLayout = Math.random() * (400 - (curr.getWidth() ) - secondary.getWidth()) + (curr.getWidth() + 20 );
+             secondary.setX(randomXLayout);
+         }
+         spriteAnimation.setCycleCount(Timeline.INDEFINITE);
+         spriteAnimation.play();
+         pane = new Pane(bg, curr, sprite, secondary);
+         scene = new Scene(pane, 400, 600);
+         gameLoop(scene);
+         stage.setScene(scene);
+         stage.show();
+
+
+
+    }
+    private static void gameLoop(Scene scene){
+
+        T2 t2 = new T2();
         if (secondary == null) {
-            secondary = createBlock();
-            double randomXLayout = Math.random() * (400 - (curr.getWidth() ) - secondary.getWidth()) + (curr.getWidth() + 10 );
+            secondary = new Rectangle();
+            double randomWidth = Math.random() * (200 - 100);
+            secondary.setHeight(200);
+            secondary.setWidth(randomWidth);
+            secondary.setArcHeight(5.0);
+            secondary.setArcWidth(5.0);
+            double randomXLayout = Math.random() * (400 - (curr.getWidth()) - secondary.getWidth()) + (curr.getWidth() + 30);
             secondary.setX(randomXLayout);
+            secondary.setY(600 - curr.getHeight());
+            secondary.setFill(Color.web("#0d0d0d"));
+            secondary.setStroke(Color.YELLOW);
+            pane.getChildren().add(secondary);
         }
-
-        Timeline spriteAnimation = new Timeline(
-                new KeyFrame(Duration.millis(100), event -> animateSprite(sprite))
-        );
-
-        spriteAnimation.setCycleCount(Timeline.INDEFINITE);
-        spriteAnimation.play();
-
-        scene = new Scene(new Group(bg, curr, sprite, secondary), 400, 600);
-
-        scene.setOnKeyPressed(event -> {
+        Game.scene.setOnKeyPressed(event -> {
             if (event.getCode() == KeyCode.SPACE && !spaceBarPressed) {
                 if(!t2.isAlive() || !isrunning) {
                     spaceBarPressed = true;
                     if (stick == null) {
-                        //translateBlocks();
-                        startStickGrowth(scene, curr);
+                        startStickGrowth(Game.scene, curr);
                         spaceBarPressed = false;
                     }
                 }
             }
         });
 
-        scene.setOnKeyReleased(event -> {
+        Game.scene.setOnKeyReleased(event -> {
             if (event.getCode() == KeyCode.SPACE) {
                 if(!isrunning) {
                     cangrow = false;
@@ -100,8 +117,7 @@ public class Game  {
             }
         });
 
-        stage.setScene(scene);
-        stage.show();
+
     }
     private static void moveSprite(){
         sprite.setImage(new Image("MOVE.png"));
@@ -121,18 +137,16 @@ public class Game  {
         spriteAnimation.setCycleCount(3);
         spriteAnimation.play();
     }
-
     private static void startStickGrowth(Scene scene, Rectangle curr) {
         stick = new Rectangle();
         stick.setWidth(5);
         stick.setHeight(0);
         stick.setFill(Color.web("#0d0d0d"));
         stick.setArcWidth(5.0);
-        stick.setX(sprite.getX() + sprite.getFitWidth() -2);
+        stick.setX(curr.getX() + curr.getWidth() - 2);
         stick.setY(curr.getY());
 
-        Group root = (Group) scene.getRoot();
-        root.getChildren().add(stick);
+        pane.getChildren().add(stick);
 
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.millis(10), event -> {
@@ -145,15 +159,6 @@ public class Game  {
 
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
-    }
-    private static Rectangle updateBlock(Rectangle curr) {
-        //double randomWidth = 200 - (secondary.getWidth() + 10);
-        curr.setWidth(Math.random() * (200 - (sprite.getFitWidth())) + 10);
-
-        double maxXLayout = 400 - curr.getWidth();
-        curr.setX(Math.random() * maxXLayout);
-
-        return curr;
     }
     private static int angle = 0;
     private static Timeline rotationTimeline = null;
@@ -185,44 +190,36 @@ public class Game  {
         rotationTimeline.play();
 
     }
-
     private static void dies() {
         TranslateTransition death = new TranslateTransition(Duration.seconds(1),sprite);
         death.setToY(600);
         death.play();
+        death.setOnFinished(event->{
+            //code for end screen
+            //create a rectangle
+            //set rectangle opacity to 0.7
+            //create labels,buttons
+            //set their positions in there
+            //thats about it
+        });
     }
-
     private static void translateSpriteToTip(double tipX) {
         moveSprite();
         TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(1), sprite);
         translateTransition.setToX(tipX+30);
-
+        System.out.println(stick.getX()+tipX);
+        System.out.println(secondary.getX());
+        System.out.println(secondary.getX()+secondary.getWidth());
         if ((tipX + stick.getX() <= (secondary.getX() + secondary.getWidth()) && tipX + stick.getX() >= (secondary.getX()))) {
+            System.out.println("landed");
             if(tipX + stick.getX() < (secondary.getX()+secondary.getWidth()/2)){
                 translateTransition.setToX(secondary.getX());
             }
             translateTransition.setOnFinished(event -> sprite.setImage(new Image("IDLE.png")));
             translateTransition.setOnFinished(event -> {
-                try {
-                    translateBlocks();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+                translateBlocks();
             });
 
-        }else if((tipX + stick.getX() <= (curr.getX() + curr.getWidth()) && tipX + stick.getX() >= (curr.getX()))){
-            if(tipX + stick.getX() < (secondary.getX()+secondary.getWidth()/2)){
-                translateTransition.setToX(secondary.getX());
-            }
-            translateTransition.setOnFinished(event -> sprite.setImage(new Image("IDLE.png")));
-            translateTransition.setOnFinished(event ->
-            {
-                try {
-                    translateBlocks();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            });
         }
         else{
             translateTransition.setOnFinished(event -> dies());
@@ -231,9 +228,6 @@ public class Game  {
         translateTransition.play();
 
     }
-
-
-
     private static void stopRotation(){
         rotationTimeline.stop();
         rotationTimeline = null;
@@ -247,97 +241,59 @@ public class Game  {
 
     private static Rectangle createBlock() {
         Rectangle x = new Rectangle();
-        double randomWidth = Math.random() * (200 - 100) + 10;
-        x.setWidth(randomWidth);
+        double randomWidth = Math.random() * (200 - 100);
+        x = new Rectangle();
         x.setHeight(200);
+        x.setWidth(randomWidth);
         x.setArcHeight(5.0);
         x.setArcWidth(5.0);
-
-        // Ensure the block is placed in the open space after curr's width + 10
-
-        x.setX(400);
-        x.setY(400);
-
+        double randomX = Math.random() * (400 - 210);
+        x.setX(randomX);
+        x.setY(600 - curr.getHeight());
         x.setFill(Color.web("#0d0d0d"));
         x.setStroke(Color.WHITE);
         return x;
     }
-
-    private static void translateBlocks() throws InterruptedException {
-        /*
-        System.out.println("HELLO");
+    private static Timeline translateBlocksTimeline = null;
+    private static void translateBlocks() {
         sprite.setImage(new Image("IDLE.png"));
+        translateBlocksTimeline = new Timeline(
+                new KeyFrame(Duration.millis(5),event -> {
+                    if (secondary.getX() <= 0) {
+                        stopTranslation();
+                    }else{
+                        curr.setTranslateX(curr.getTranslateX() - 1);
+                        secondary.setX(secondary.getX() - 1);
+                        sprite.setTranslateX(sprite.getTranslateX() - 1);
+                        if (stick != null) {
+                            stick.setTranslateX(stick.getTranslateX() - 1);
+                        }
+                    }
+                })
+        );
+        translateBlocksTimeline.setCycleCount(Timeline.INDEFINITE);
+        translateBlocksTimeline.play();
+       }
 
-        //moving the previous block outside the screen to the left (stage left)
-        TranslateTransition prevBlock = new TranslateTransition(Duration.seconds(1),curr);
-        prevBlock.setToX(-400);
-        prevBlock.setOnFinished(event -> curr = null);
+    private static void stopTranslation() {
+        translateBlocksTimeline.stop();
+        translateBlocksTimeline = null;
 
-        TranslateTransition currBlock = new TranslateTransition(Duration.seconds(1), curr=secondary);
-        currBlock.setToX(0);
-        currBlock.setOnFinished(event -> {curr.setStroke(Color.RED);secondary = createBlock();});
+        // Update the properties of curr to match secondary
 
-        TranslateTransition spriteBlock = new TranslateTransition(Duration.seconds(1), sprite);
-        spriteBlock.setToX(curr.getWidth() / 2);
-
-        TranslateTransition stickBlock = new TranslateTransition(Duration.seconds(1), stick);
-        stickBlock.setToX(-400);
-        stickBlock.setOnFinished(event -> stick = null);
-
-        double randomXLayout = Math.random() * (400 - secondary.getWidth());
-        TranslateTransition newBlock = new TranslateTransition(Duration.seconds(1), secondary);
-        newBlock.setToX(randomXLayout);
-        newBlock.setOnFinished(event -> {
-            secondary.setX(randomXLayout);
-            System.out.println(secondary.getX());
-            isrunning = false;
-        });
-
-        ParallelTransition parallelTransition = new ParallelTransition(prevBlock, currBlock, spriteBlock, stickBlock);
-        parallelTransition.setOnFinished(event -> newBlock.play());
-        parallelTransition.play();
-
-        //move the background
-
-        // moving the current block to the left part of the screen
-        */
-        /*
-        TranslateTransition currBlock = new TranslateTransition(Duration.seconds(1),secondary);
-        currBlock.setOnFinished(event -> {curr = secondary;secondary  = createBlock();});
-
-        //moving the sprite back
-        TranslateTransition spriteBlock = new TranslateTransition(Duration.seconds(1),sprite);
-        spriteBlock.setToX(0);
-
-        // moving the stick outside the screen
-        TranslateTransition stickBlock = new TranslateTransition(Duration.seconds(1),stick);
-        stickBlock.setToX(-400);
-        stickBlock.setOnFinished(event -> stick = null);
-        if (secondary.getWidth() > 100){
-            currBlock.setToX(100 - secondary.getWidth() );
-        }else{
-            currBlock.setToX(0);
-        }
-
-        ParallelTransition parallelTransition = new ParallelTransition(prevBlock,spriteBlock,stickBlock,currBlock);
-        //parallelTransition.setOnFinished(event -> secondary = createBlock());
-        parallelTransition.play();
-
-        // getting in the newly created block
-        double randomXLayout = Math.random() * (400 - (curr.getWidth() ) - secondary.getWidth()) + (curr.getWidth() + 10 );
-        TranslateTransition newBlock = new TranslateTransition(Duration.seconds(1),secondary);
-        newBlock.setToX(randomXLayout);
-        newBlock.setOnFinished(event -> isrunning = false);
-        parallelTransition.setOnFinished(event -> newBlock.play());*/
-        translateBlock(secondary,0);
+        curr = secondary;
+        secondary = null;
 
 
+
+        // Reset stick
+        stick = null;
+
+        curr.setStroke(Color.RED);
+        isrunning = false;
+        gameLoop(scene);
     }
-    private static void translateBlock(Rectangle rectangle, int to) throws InterruptedException {
-        T1 t1 = new T1(rectangle,to);
-        Thread t = new Thread(t1);
-        t.start();
-    }
+
 
 
 }
